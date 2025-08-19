dfdc0029a81142f9966643489270750d900662c1
Isaac-code <isaac.cade26@gmail.com>
2025-08-19 01:24:58 -0300

    acount

---
+++ Constructors_Overloading_Encapsulation/Ex_03/Acount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_03
+{
+    internal class Acount
+    {
+        public int Id { get; private set; }
+        public double Saldo { get; private set; }
+        public string User { get; private set; }
+
+
+        public Acount(int id,string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Acount(int id,string user,double saldo) : this(id,user) 
+        {
+            Saldo = saldo;
+        }
+
+       
+        
+        public void Deposit(double dolar)
+        {
+            Saldo += dolar;
+        }
+
+        public void Sake(double dolar)
+        {
+            double tax = 5.0;
+            Saldo -= dolar;
+            Saldo -= tax;
+        }
+
+        public override string ToString() {
+            return " Usuario:" + User
+                + " Conta:" + Id 
+                + " Saldo:"+ Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- Constructors_Overloading_Encapsulation/Ex_03/Program.cs
+++ Constructors_Overloading_Encapsulation/Ex_03/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+
+
+namespace Ex_03
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Acount user;
+
+            Console.Write("Informe o numero da conta: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o titular da conta: ");
+            string name = Console.ReadLine();   
+
+            Console.Write("Deseja efetuar um deposito inicial ? (s/n)");
+            char op = char.Parse(Console.ReadLine());
+
+            if(op == 's')
+            {
+                
+                Console.Write("Informe quanto vc deseja depositar: ");
+                double dep = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                user = new Acount(id,name,dep);
+                Console.WriteLine(user);
+            }
+            else
+            {
+                user = new Acount(id, name);
+                Console.WriteLine(user);
+            }
+
+
+            Console.WriteLine("Informe um valor para deposito");
+            double deposit = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            user.Deposit(deposit);
+
+   
+            Console.WriteLine("Dados da conta atualizados:\n" + user);
+
+            Console.WriteLine("Informe um valor para saque");
+            double sake = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            user.Sake(sake);
+
+
+            Console.WriteLine("Dados da conta atualizados:\n" + user);
+        }
+
+    }
+
+
+}
+
+
