cf6c8cacaf90b723a05131355fa16c0126176d17
Isaac-code <isaac.cade26@gmail.com>
2025-08-27 00:30:06 -0300

    Ex_01-fileinfo

---
+++ Working_with_files/Working_with_files/Ex_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\text\one.txt";
+            string targetPath = @"c:\text\two.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred:" + e.Message);
+            }
+        }
+    }
+}
