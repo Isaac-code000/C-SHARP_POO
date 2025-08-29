5bb23a09b0a16f6c5450ce874a21d803a247817b
Isaac-code <isaac.cade26@gmail.com>
2025-08-29 17:04:11 -0300

    Ex_02

---
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ex_01.Entities;
+
+namespace Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintService<int> ps = new PrintService<int>();
+            Console.WriteLine("How many values ?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter a number:");
+                int value = int.Parse(Console.ReadLine());
+                ps.Add(value);
+            }
+            ps.Print();
+            Console.WriteLine("First: " + ps.First);
+        }
+    }
+}
