943a4faa8a630f6ee24cecbf9dfe855aa7f68c17
Isaac-code <isaac.cade26@gmail.com>
2025-08-30 22:25:40 -0300

    Ex_04-HashSet and SortedSet

--- Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_03/Program.cs
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_03/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-
-
-
-using System.Collections.Generic;
 using Ex_03.Entities;
 namespace Ex_03
 {
---
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_04/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> fruits = new HashSet<string>
+            {
+                "apple",
+                "banana",
+                "orange",
+                "grape",
+                "kiwi"
+            };
+
+            SortedSet<int> a = new SortedSet<int> { 1, 2, 3, 4, 5 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 4, 7, 9, 10 };
+            SortedSet<int> c = new SortedSet<int>(a);
+
+            c.UnionWith(b);
+
+            Console.WriteLine("Union a with b");
+            PrintSet(c);
+
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            Console.WriteLine("Intersect a with b");
+            PrintSet(d);
+
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            Console.WriteLine("Except a with b");
+            PrintSet(e);
+
+
+            Console.WriteLine(fruits.Contains("apple"));
+            foreach (string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+        }
+
+        static void PrintSet<T>(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
