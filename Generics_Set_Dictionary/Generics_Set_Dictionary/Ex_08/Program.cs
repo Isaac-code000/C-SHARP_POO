27ff21d98203f5be1319799ccb6842888531dbaa
Isaac-code <isaac.cade26@gmail.com>
2025-09-01 16:41:46 -0300

    Ex_08,09 Dictionarys

---
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Ex_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> students = new Dictionary<string, string>();
+
+            students["isaac"]= "isaac.@gmail.com";
+            students["maria"] = "maria.@gmail.com";
+            students["ana"] = "ana.@gmail.com";
+
+            Console.WriteLine(students["isaac"]);
+
+            students["isaac"] = "isaac_new.@gmail.com";
+
+            Console.WriteLine(students["isaac"]);
+
+
+            foreach(KeyValuePair<string,string> item in students)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+
+        }
+    }
+}
---
+++ Generics_Set_Dictionary/Generics_Set_Dictionary/Ex_09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Channels;
+
+
+
+namespace Ex_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the path of file");
+            string path = Console.ReadLine();
+
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> users = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        int number = int.Parse(fields[1]);
+                        if (users.ContainsKey(name))
+                        {
+                            users[name] += number;
+                        }
+                        else
+                        {
+                            users[name] = number;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> item in users)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+
+                }
+
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error as ocurred: " + e.Message);
+            }
+
+        }
+    }
+}
